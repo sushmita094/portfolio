f016514233a41314de12e0c5b69bc35662a13448
Sushmita <sushmita094@gmail.com>
2021-04-23 10:50:22 +0530

    ADD blog api integration

--- app.js
+++ app.js
@@ -3,3 +3,56 @@ const scrollToSection = (section) => {
   // let element = document.getElementById(section);
   // element.scrollIntoView();
 };
+
+const query = `
+{
+  user(username:"sushmita") {
+    publication {
+      posts {
+        title
+        brief
+        slug
+        dateAdded
+        tags {
+          name
+          _id
+        }
+      }
+    }
+  }
+}`;
+
+async function getData() {
+  const response = await fetch("https://api.hashnode.com", {
+    method: "post",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ query: query }),
+  });
+  const body = await response.json();
+  let html = "";
+
+  body.data?.user?.publication?.posts.forEach((post) => {
+    const title = `<p class="blog-title">${post.title}</p>`;
+    const desc = `<p class="desc">${post.brief}</p>`;
+    let date = new Date(post.dateAdded);
+    date = `<p class='date-posted'>${date.getDate()} ${
+      date.getMonth() + 1
+    } ${date.getFullYear()}</p>`;
+    html += `<a class="blog-card-link">
+    <div class="blog-card">
+    <div>
+    ${title}
+    ${desc}
+    </div>
+    <div>
+    ${date}
+    </div>
+    </div>
+    </a>`;
+  });
+  document.getElementById("blogPosts").innerHTML = html;
+}
+
+getData();
